d0936a6a2b21fd2b49def443aad8d899810b7167
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-11-22 21:53:51 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Awwwards Example 1
+
+Made with React and TailwindCSS.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="text-3xl font-bold font-circular-web text-violet-300">
+            Hello World
+        </div>
+    );
+};
+
+export default App;
